fd22324b13c7cc72c7c9b6bf1c0fc640b85a23ad
Neto Rissi <netorissi@hotmail.com>
2021-06-20 15:47:52 -0300

    feat: added tests to logo component

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,4 +1,7 @@
 import { screen } from '@testing-library/react';
+import media from 'styled-media-query';
+import 'jest-styled-components';
+
 import { renderWithTheme } from 'utils/tests/helpers';
 
 import theme from 'styles/theme';
@@ -20,5 +23,33 @@ describe('<Logo/>', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: theme.colors.black
     });
+  });
+
+  it('should render a normal logo', () => {
+    renderWithTheme(<Logo />);
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    });
+  });
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />);
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    });
+  });
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo size="large" hideOnMobile />);
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    );
   });
 });
